8244d01aea77b78b9dea217a060b8d3be6e6584e
Jaddal0 <136065037+Jaddal0@users.noreply.github.com>
2023-06-09 01:05:09 -0700

    RightTriangle.cs

--- RightTriangle.cs
+++ RightTriangle.cs
@@ -1,1 +1,21 @@
-﻿
+using System;
+using static System.Console;
+
+class Asterisk
+{
+    static void Main()
+    {
+
+        for (int i = 10; i > 0; i--)
+        {
+
+            for (int j = 0; j < i; j++)
+            {
+                Write("*");
+            }
+
+            Write("\n");
+
+        }
+    }
+}
