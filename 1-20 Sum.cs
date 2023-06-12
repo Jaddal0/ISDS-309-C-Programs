954743558532d8919df220f337ec29f4fe8a787c
Jaddal0 <136065037+Jaddal0@users.noreply.github.com>
2023-06-12 11:06:48 -0700

    Add files via upload

---
+++ 1-20 Sum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+
+/*
+Write a program to display integers from 1 to 20, and then display their sum
+-using a while loop
+-using a for loop
+*/
+
+class idk
+{
+    static void Main()
+    {
+        int num = 1;
+        while (num <= 20)
+        {
+            Write("{0} ", num);
+            num++;
+        }
+
+        int sum = 0;
+        for (int i = 1; i <= 20; i++)
+        {
+            sum = sum + i;
+        }
+        WriteLine("\nSum is {0}", sum);
+    }
+}
