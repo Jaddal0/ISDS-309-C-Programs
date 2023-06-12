7d0c5885f2d8f684e7689b8cd60826c39433e3bc
Jaddal0 <136065037+Jaddal0@users.noreply.github.com>
2023-06-12 11:26:29 -0700

    Add files via upload

---
+++ Do-While Loop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console;
+class LoopingBankBal
+{
+    static void Main()
+    {
+        double bankBal = 1000;
+        const double INT_RATE = 0.04;
+        char response;
+
+        do
+        {
+            Write("Bank balance is {0:C2}", bankBal);
+            bankBal = bankBal + bankBal * INT_RATE;
+            Write("\nDo you want to see next year’s balance? Y or N ...");
+            response = Convert.ToChar(ReadLine());
+        } while (response == 'Y' || response == 'y');
+        
+        WriteLine("Have a nice day!");
+
+    }
+}
