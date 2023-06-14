d35ce29d4bf09a61afe99ae57edd024a35a1a6fb
Jaddal0 <136065037+Jaddal0@users.noreply.github.com>
2023-06-14 00:55:01 -0700

    PrimeNumberTest.cs

--- PrimeNumberTest.cs
+++ PrimeNumberTest.cs
@@ -1,59 +1,29 @@
 using System;
-
 using static System.Console;
-
-
 class PrimeNumberTest
 {
+    static void Main()
+    {
+        Write("Enter an integer that is greater than or equal to 2: ");
+        int num;
+        num = Convert.ToInt32(Console.ReadLine());
+        int divisors = 0;
 
-  static void Main ()
-  {
+        for (int i = 1; i <= num; i++)
+        {
+            if (num % i == 0)
+            {
+                divisors++;
+            }
+        }
 
-    string num;
-
-      Write ("Enter an integer that is greater than or equal to 2: ");
-
-      num = ReadLine ();
-
-    int numInput = Convert.ToInt32 (num);
-
-
-    while (numInput > 2)
-
-      {
-
-	Write ("Enter an integer that is greater than or equal to 2: ");
-
-      }
-
-
-    if (numInput == 1 || numInput == 0)
-
-      {
-
-      Write ("{0} is not a Prime number", numInput);}
-
-
-    else
-
-      {
-
-	for (int i = 2; i <= numInput / 2; i++)
-
-	  {
-
-	    if (numInput % i == 0)
-
-	      {
-
-	      WriteLine ("{0} is not a Prime Number", numInput);}
-
-	  }
-
-	WriteLine ("{0} is a Prime Number", numInput);
-
-      }
-
-  }
-
+        if (divisors == 2)
+        {
+            Console.WriteLine("{0} is a Prime Number", num);
+        }
+        else
+        {
+            Console.WriteLine("{0} is a not a Prime Number", num);
+        }
+    }
 }
