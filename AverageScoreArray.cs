da72e2a3650c9ed0dbe7b032620bc5df5009f3eb
Jaddal0 <136065037+Jaddal0@users.noreply.github.com>
2023-06-21 23:43:06 -0700

    Add files via upload

---
+++ AverageScoreArray.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static System.Console;
+
+class AverageScoreArray
+{
+    static void Main()
+    {
+        int[] scoreArray = new int[5];
+        double sum = 0;
+
+        for (int i = 0; i < scoreArray.Length; i++)
+        {
+            Write("Enter an integer test score >> ");
+            scoreArray[i] = Convert.ToInt32(ReadLine());
+        }
+        Console.WriteLine("\nThe integers entered are: ");
+
+        for (int i = 0;i < scoreArray.Length; i++)
+        {
+            Console.WriteLine(scoreArray[i]);
+            sum += scoreArray[i];
+        }
+
+        double avg = sum / scoreArray.Length;
+        Console.WriteLine("\nThe average score is {0:0.00}\n", avg);
+    }
+}
