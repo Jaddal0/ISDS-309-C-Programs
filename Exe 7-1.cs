489edb3ccf6fa312601ab7e4ca9a3ff226708cb3
Jaddal0 <136065037+Jaddal0@users.noreply.github.com>
2023-06-26 12:09:24 -0700

    Add files via upload

---
+++ Exe 7-1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+
+class Exe_7_1
+{
+    static void Main()
+    {
+        Write("Enter the unit price in $ >> ");
+        double price = Convert.ToDouble(ReadLine());
+        Write("Enter the quantity of purchase >> ");
+        int quantity = Convert.ToInt32(ReadLine());
+        CalSales(double price, int quantity);
+    }
+    private static void CalSales(double price, int quantity)
+    {
+        double product = price * quantity;
+        Console.WriteLine("The total purchase is {0:C}", product);
+    }
+}
